75747834851d8959c99976b3d6a2a584fe4007a3
Elisee <mugiranezaelisee0@gmail.com>
2026-01-09 10:13:16 -0800

    update lamp status when ON or OFF button is clicked

--- lamp.py
+++ lamp.py
@@ -1,65 +1,64 @@
 import tkinter as tk
 
 root = tk.Tk()
-root.title("Lamp control")
+root.title("Lamp Control")
+
+message = None  
 
 def check_credentials():
     username = username_entry.get()
     password = password_entry.get()
-    if(username == "Elisee" and password == "jefazo"):
+    if username == "Elisee" and password == "jefazo":
         root.withdraw()
         open_dashboard()
+    else:
+        print("Wrong credentials")
 
 def open_dashboard():
+    global message
+
     dashboard = tk.Toplevel(root)
-    dashboard.title("Control lamp")
+    dashboard.title("Control Lamp")
 
     on_button = tk.Button(
         dashboard,
         text="ON",
+        width=10,
         command=toggle_on
     )
-    on_button.pack()
+    on_button.pack(pady=5)
 
     off_button = tk.Button(
         dashboard,
         text="OFF",
-        command = toggle_off
-        # message.config(text="Lamp ON")
+        width=10,
+        command=toggle_off
     )
-    off_button.pack()
-    message = tk.Label(dashboard, text="")
-    message.pack()
+    off_button.pack(pady=5)
+
+    message = tk.Label(dashboard, text="Lamp is OFF", fg="red")
+    message.pack(pady=10)
 
 def toggle_on():
-    # message.config(dashboard, text="Lamp ON")
+    message.config(text="Lamp is ON", fg="green")
     print("ON button clicked")
 
 def toggle_off():
-    # message.config(dashboard, text="Lamp OFF")
-    print("Off button clicked")
+    message.config(text="Lamp is OFF", fg="red")
+    print("OFF button clicked")
 
-label = tk.Label(
+tk.Label(
     root,
-    text="Welcome to lamp ocontrol page!!",
+    text="Welcome to lamp control page!!",
     fg="blue"
-    ).pack()
+).pack(pady=10)
 
-label = tk.Label(
-    root,
-    text="Username"
-    ).pack()
-
+tk.Label(root, text="Username").pack()
 username_entry = tk.Entry(root)
 username_entry.pack()
 
-label = tk.Label(root,
-    text="Password"
-    ).pack()
-password_entry = tk.Entry(
-    root,
-    show="*"
-    )
+tk.Label(root, text="Password").pack()
+password_entry = tk.Entry(root, show="*")
 password_entry.pack()
 
 login_button = tk.Button(
@@ -67,6 +66,6 @@ login_button = tk.Button(
     text="Login",
     command=check_credentials
 )
-login_button.pack()
+login_button.pack(pady=10)
 
 root.mainloop()
